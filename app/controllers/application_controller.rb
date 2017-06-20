3b867acdb9434a1363fe1259f3e6b886be40e1a5
eera k m <fgalore414@gmail.com>
2017-06-20 04:25:27 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: "News today in all channels: Otto Warmbier passed away. June 19th/20th 2017"
+  end
 end
