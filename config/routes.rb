5feb26455e7d3834f5d17f5050449ed845d64e32
FGALORE <fgalore414@gmail.com>
2017-06-30 06:14:00 +0000

    I

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  get 'foo/bar'
+  #get 'foo/bar'
 
-  get 'foo/baz'
+  #get 'foo/baz'
  root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get  'static_pages/home', to: 'static_pages#home'
+  get  'static_pages/help',    to: 'static_pages#help'
+  get  'static_pages/about',   to: 'static_pages#about'
+  get  'static_pages/contact', to: 'static_pages#contact'
 
   get  'static_pages/signup',  to: 'users#new'
  
